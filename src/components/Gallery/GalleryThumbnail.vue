3c67f364c85729725f81af31a990698785c61ed3
José Luis Pereira <pepelui@outlook.com>
2022-03-29 15:32:03 -0300

    Change background color

--- src/components/Gallery/GalleryThumbnail.vue
+++ src/components/Gallery/GalleryThumbnail.vue
@@ -1,6 +1,6 @@
 <template>
   <div 
-    class="ml-1 mr-1 mt-2 bg-black flex flex-col justify-center cursor-pointer w-24 h-24"
+    class="ml-1 mr-1 mt-2 bg-white flex flex-col justify-center cursor-pointer w-24 h-24 border"
     :title="title"
   >
     <img 
